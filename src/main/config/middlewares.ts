8c05940f8de4c81aedc975a4602d0c6761b18288
Gustavo Fraga <gustavohenriquemiele@live.com>
2021-12-08 14:35:22 -0300

    feat: ensure requests return json as deault content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { bodyParser } from '../middleware/body-parser';
+import { contentType } from '../middleware/content-type';
 import { cors } from '../middleware/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 };
