ef08748c5269b3ed79a318042cb1437a6eac5117
Gustavo Fraga <gustavohenriquemiele@live.com>
2022-03-01 00:38:44 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,20 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
 
   async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__);
+    this.client = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
   },
 
   async disconnect(): Promise<void> {
     this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
   }
 };
