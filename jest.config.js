943bf0e76d0e584083b1e366c22dbebb515e6e34
Gustavo Fraga <gustavohenriquemiele@live.com>
2022-03-01 00:38:00 -0300

    refactor: change mongodb version and fix jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
