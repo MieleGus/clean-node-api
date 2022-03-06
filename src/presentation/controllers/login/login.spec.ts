d5796e749d4961922b7f95d0d56d878ff9330a85
Gustavo Fraga <gustavohenriquemiele@live.com>
2022-03-05 22:59:03 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,7 +2,7 @@ import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { EmailValidator } from '../signup/signup-protocols';
 import { LoginController } from './login';
-
+import { HttpRequest } from '../../protocols'
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid(email: string): boolean {
@@ -11,6 +11,13 @@ const makeEmailValidator = (): EmailValidator => {
   }
   return new EmailValidatorStub();
 };
+
+const makeFakeRequest = (): HttpRequest => ({
+    body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+    }
+})
 
 interface SutTypes {
   sut: LoginController;
@@ -64,12 +71,7 @@ describe('Login Controller', () => {
   test('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    };
+    const httpRequest = ;
     await sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
