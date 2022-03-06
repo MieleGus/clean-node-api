745afc10b3ccd3483ac7454d2ce10cdf85031a11
Gustavo Fraga <gustavohenriquemiele@live.com>
2022-03-05 23:06:56 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, success } from '../../helpers/http-helper';
+import { badRequest, serverError, success } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
@@ -11,17 +11,21 @@ export class LoginController implements Controller {
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return badRequest(new MissingParamError('email'));
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return badRequest(new MissingParamError('email'));
+      }
+      if (!password) {
+        return badRequest(new MissingParamError('password'));
+      }
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+      return success('xd');
+    } catch (error) {
+      return serverError(error);
     }
-    if (!password) {
-      return badRequest(new MissingParamError('password'));
-    }
-    const isValid = this.emailValidator.isValid(email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
-    }
-    return success('xd');
   }
 }
